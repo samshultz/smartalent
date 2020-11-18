c8dc8a38d2609f04cca5021669b4727f9bdb7f77
Samuel <taiwogabrielsamuel@gmail.com>
2020-11-18 08:28:44 +0000

    cleaned up the emailauthBackend

--- account/authentication.py
+++ account/authentication.py
@@ -1,20 +1,28 @@
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
-class EmailAuthBackend(object):
-	"""
-	Authenticate using an e-mail address.
-	"""
-	def authenticate(self, request, username=None, password=None):
-		try:
-			user = get_user_model().objects.filter(email=username).first()
-			if user.check_password(password):
-				return user
-			return None
-		except get_user_model().DoesNotExist:
-			return None
 
-	def get_user(self, user_id):
-		try:
-			return get_user_model().objects.get(pk=user_id)
-		except get_user_model().DoesNotExist:
-			return None
+UserModel = get_user_model()
+
+
+class EmailAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserModel.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username))
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)
+        except MultipleObjectsReturned:
+            return User.objects.filter(email=username).order_by('id').first()
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+
+    def get_user(self, user_id):
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
+
+        return user if self.user_can_authenticate(user) else None
